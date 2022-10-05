ae947d4a945936da057d2bc32e6e701bcc2d3c25
Henrikochh <113605484+Henrikochh@users.noreply.github.com>
2022-10-05 15:30:29 +0200

    Add files via upload

---
+++ Code to correct/main.c
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Henri Koch
+ *
+ * Created on 5 October 2022, 13:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define OKAY 5
+
+/*
+ * 
+ */
+int main() {
+    
+    
+    printf("Dies ist ein Code Beispiel von Henri Koch. Bitte nehmen sie dieses Code Beispiel nicht als Vorlage, da es ganz viele verschiedene clean code fehler aufweist. Viel Spass beim finden der Fehler!");printf("\n\nFangen wir doch mit einer einfachen Rechnung an und zwar mit einer Adition.");
+    
+    int a = 4;
+    int x = 7;
+    
+  printf("\n\nDas Ergebniss von 7 + 4 ist: %d", x + a)
+;
+    
+    int b;
+    
+printf("\n\nDanke fuers leosen der Reatsel")    
+    
+    
+    
+    
+;    
+    return (EXIT_SUCCESS);
+}
+
